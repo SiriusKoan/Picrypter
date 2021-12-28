9a38eda3d5212886dff247fe392fb84433ccdbdd
SiriusKoan <k.peihsun@gmail.com>
2021-12-28 17:19:47 +0800

    Add some basic functions

--- functions.py
+++ functions.py
@@ -1,5 +1,8 @@
+from expand_matrix import M
+from scipy.linalg import null_space
+
 def encrypt(image, password):
-    key = convert_password_into_key(password)
+    key = convert_password_into_key(np.array([ord(c) for c in password]), len(password))
     matrix = make_transform_matrix(key)
     encrypted_image = transform_image(image, matrix)
     return encrypted_image
@@ -8,22 +11,23 @@ def encrypt(image, password):
 def decrypt(image, password):
     key = convert_password_into_key(password)
     matrix = make_transform_matrix(key)
-    # make inverse
+    matrix = numpy.linalg.inv(matrix)
     decrypted_image = transform_image(image, matrix)
     return decrypted_image
 
-
-def convert_password_into_key(password):
-    pass
+def convert_password_into_key(password, length):
+    submatrix = M[:, :length]
+    return submatrix @ password
 
 
 def make_transform_matrix(key):
     pass
 
 
-def check_independent(matrix, vector):
-    pass
+def check_independent(matrix):
+    ns = null_space(matrix)
+    return bool(ns)
 
 
 def transform_image(image, matrix):
-    pass
+    return matrix @ image
