d155f8d980f3a7a598c9b683bf0c08492749fff9
LBWei <0800680274united@gmail.com>
2021-12-28 17:17:06 +0800

    func: m_t_m

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def encrypt(image, password):
     key = convert_password_into_key(password)
     matrix = make_transform_matrix(key)
@@ -18,10 +20,23 @@ def convert_password_into_key(password):
 
 
 def make_transform_matrix(key):
-    pass
+    LEN = 100
+    start, end = 0, LEN
+    encrypt_matrix = np.array()
+    encrypt_matrix = np.reshape(LEN*1, dtype='i')
+    while end <= 6000:
+        a_col = key[start:end]
+        tmp_mat = np.c_[encrypt_matrix, a_col]
+        check = check_independent(tmp_mat)
+        if check: 
+            encrypt_matrix = tmp_mat
+        start += LEN
+        end += LEN
+    return encrypt_matrix
 
 
-def check_independent(matrix, vector):
+
+def check_independent(matrix):
     pass
 
 
