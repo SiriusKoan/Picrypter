a1baf15bbce760a2a01732c2bfa1d2dbea01fe2b
wujoe0415 <wujoe0415@gmail.com>
2021-12-28 23:30:55 +0800

    hackathon

--- PicrypterBOT.py
+++ PicrypterBOT.py
@@ -1,11 +1,12 @@
 import json
 import os
+import requests
 import logging
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from telegram import Update, Bot ,Message
 from PIL import Image
-from functions import encrypt,decrypt
+from functions import encrypt, decrypt
 
 # import telebot
 #from telebot import types
@@ -23,8 +24,8 @@ def start(update: Update, context: CallbackContext):
     /start - Introduction.
     /end - Clear all changes.
     /password - The random number sender and receiver choose.
-    /encrypt - Encrypt your image according to your password.
-    /Decrypt - Decrypt your image according to your password.
+    /encrypt - Encrypt your image according to password.
+    /decrypt - Decrypt your image according to password.
     Image : The image you long to encrypt.
     """)
 
@@ -46,6 +47,8 @@ def Encrypt(update: Update, context: CallbackContext):
 
     image = encrypt(image,password) #encrypt it
     #init blank variable
+    context.bot.send_photo(chat_id=update.message.chat_id, photo=image)
+
     end(update, context)
 
 def Decrypt(update: Update, context: CallbackContext):
@@ -55,8 +58,8 @@ def Decrypt(update: Update, context: CallbackContext):
         update.message.reply_text("Please enter the password!")
         return
 
-    image = encrypt(image,password)
-    context.bot.send_photo(chat_id=update.message.chat_id,photo=image)
+    image = decrypt(image,password)
+    context.bot.send_photo(chat_id=update.message.chat_id, photo=image)
 
     #this part is to encrypt the image that users give '''
 
@@ -70,7 +73,14 @@ def Password(update: Update, context: CallbackContext):
 
 def initImage(update: Update, context: CallbackContext):
     global image
-    image = update.message.photo[-1] # the type is string PhotoType
+    global size
+    image = context.bot.getFile(update.message.photo[-1].file_id) # the type is string PhotoType
+    # print(image,dir(image))
+    # print(update.message.photo[-1], dir(update.message.photo[-1]))
+    size = (update.message.photo[-1].width, update.message.photo[-1].height)
+    image = requests.get(image.file_path).content
+    # print(image)
+    # image = context.bot.getFile(update.message.photo[-1].file_id)
     update.message.reply_text("Please enter the password.\nFormat : \"/password <password>\".")
 
     #image = Image.open("test.jpg")
@@ -81,7 +91,7 @@ def main():
     updater.dispatcher.add_handler(CommandHandler("start", start))
     updater.dispatcher.add_handler(CommandHandler("end", end))
     updater.dispatcher.add_handler(CommandHandler("encrypt",Encrypt))
-    updater.dispatcher.add_handler(CommandHandler("encrypt",Decrypt))
+    updater.dispatcher.add_handler(CommandHandler("decrypt",Decrypt))
     updater.dispatcher.add_handler(MessageHandler(Filters.photo, initImage))
     updater.dispatcher.add_handler(CommandHandler("password",Password))
 
--- functions.py
+++ functions.py
@@ -2,52 +2,76 @@ from expand_matrix import M
 from scipy.linalg import null_space
 import numpy as np
 from PIL import Image
+from io import BytesIO
 
-def encrypt(image, password, size):
-    key = convert_password_into_key(np.array([ord(c) for c in password]), len(password))
+def encrypt(image, password):
+    key = convert_password_into_key(np.array([ord(c) - 45 for c in password]), len(password))
     matrix = make_transform_matrix(key)
-    image = np.array(Image.frombytes("RGBA", size, image))
+    image = np.array(Image.open(BytesIO(image)))
     encrypted_image = transform_image(image, matrix)
     encrypted_image = Image.fromarray(encrypted_image)
-    return encrypted_image
+    print(encrypted_image, dir(encrypted_image))
+    f = BytesIO()
+    encrypted_image.save(f, "PNG")
+    return f.getvalue()
 
-def decrypt(image, password, size):
-    key = convert_password_into_key(np.array([ord(c) for c in password]), len(password))
+def decrypt(image, password):
+    key = convert_password_into_key(np.array([ord(c) - 45 for c in password]), len(password))
     matrix = make_transform_matrix(key)
-    matrix = numpy.linalg.inv(matrix)
-    image = np.array(Image.frombytes("RGBA", size, image))
+    matrix = np.linalg.inv(matrix)
+    print("inverse: ", matrix)
+    image = np.array(Image.open(BytesIO(image)))
     decrypted_image = transform_image(image, matrix)
     decrypted_image = Image.fromarray(decrypted_image)
-    return decrypted_image
+    f = BytesIO()
+    decrypted_image.save(f, "PNG")
+    return f.getvalue()
 
 def convert_password_into_key(password, length):
-    submatrix = M[:, :length]
+    
+    size = M.shape
+    for i in range(size[0]):
+        for j in range(size[1]):
+                M[i,j] = 1
+    
+    submatrix = M[:600, :length]
+    # print("submatrix: ", submatrix @ password)
     return submatrix @ password
 
 
 def make_transform_matrix(key):
-    LEN = 100
+    LEN = 1
     start, end = LEN, LEN*2
     encrypt_matrix = np.array(key[0:LEN])
     encrypt_matrix = encrypt_matrix.reshape(LEN, 1)
     
-    while end <= 6000:
+    while end <= 600:
         a_col = np.array(key[start:end])
         tmp_mat = np.c_[encrypt_matrix, a_col]
         check = check_independent(tmp_mat)
-        if check:
+        print("encrypted matrix: ", encrypt_matrix)
+        # print(np.linalg.det(tmp_mat.transpose() @ tmp_mat))
+        if not check:
+            print("tmp: ", tmp_mat)
             encrypt_matrix = tmp_mat
         start += LEN
         end += LEN
+    encrypt_matrix = encrypt_matrix.transpose() @ encrypt_matrix
+    print("encrypted matrix: ", encrypt_matrix)
+    print(np.linalg.det(encrypt_matrix))
+    # print(encrypt_matrix)
+    print(encrypt_matrix.shape)
     return encrypt_matrix
 
 
 def check_independent(matrix):
     ns = null_space(matrix)
-    return bool(ns)
+    # print(ns)
+    return bool(ns.any())
 
 
 def transform_image(image, matrix):
+    image = image.astype('float64')
     size = image.shape
     LEN = matrix.shape[0]
     start_c, end_c, start_r, end_r = 0, LEN, 0, LEN
@@ -55,9 +79,19 @@ def transform_image(image, matrix):
         start_r, end_r = 0, LEN
         while end_r <= size[1]:
             for i in range(image.shape[2]):
+                # print(image[start_c:end_c, start_r:end_r, i], matrix)
                 image[start_c:end_c, start_r:end_r, i] = image[start_c:end_c, start_r:end_r, i] @ matrix
             start_r += LEN
             end_r += LEN
         start_c += LEN
         end_c += LEN
+    print("before %")
+    print(image)
+    # for i in range(size[0]):
+    #     for j in range(size[1]):
+    #         for k in range(size[2]):
+    #             image[i,j,k] %= 256
+    print("after %")
+    print(image)
+    image = image.astype('uint8')
     return image
